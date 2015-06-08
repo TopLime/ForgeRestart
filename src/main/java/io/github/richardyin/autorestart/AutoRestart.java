213c544f174706e70976d9199ed1b39ff5ab555c
Richard Yin <richard.hongyi.yin@gmail.com>
2015-06-07 23:20:04 -0400

    Added server stopping and finished time calculation

--- src/main/java/io/github/richardyin/autorestart/AutoRestart.java
+++ src/main/java/io/github/richardyin/autorestart/AutoRestart.java
@@ -1,11 +1,18 @@
 package io.github.richardyin.autorestart;
 
+import java.util.Calendar;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.transformers.ForgeAccessTransformer;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = AutoRestart.MODID, version = AutoRestart.VERSION)
 public class AutoRestart {
@@ -15,15 +22,29 @@ public class AutoRestart {
     private long delay = 0;
     
     @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	//Calculate delay based on config file input
+    	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+    	config.load();
+    	int hrs = config.getInt("Hours", "Time", -1, -1, 23, "Hour of the day; 0-23; -1 to disable");
+    	int mins = config.getInt("Minutes", "Time", 0, 0, 59, "Minute of the day; 0-59");
+    	Calendar stopTime = Calendar.getInstance();
+    	stopTime.set(Calendar.MINUTE, mins);
+    	stopTime.set(Calendar.HOUR, hrs);
+    	Calendar nowTime = Calendar.getInstance();
+    	if(nowTime.getTimeInMillis() > stopTime.getTimeInMillis())
+    		stopTime.add(Calendar.DATE, 1); //stop tomorrow if past stop time
+    	delay = stopTime.getTimeInMillis() - nowTime.getTimeInMillis();
+    }
+    
+    @EventHandler
     public void init(FMLInitializationEvent event) {
-    	//TODO Calculate delay based on config file input
-    	
     	//Schedule event
         Executors.newSingleThreadScheduledExecutor().schedule(new Runnable() {
 			@Override
 			public void run() {
-				//something here
+				MinecraftServer.getServer().stopServer();
 			}
-        }, delay, TimeUnit.SECONDS);
+        }, delay, TimeUnit.MILLISECONDS);
     }
 }
